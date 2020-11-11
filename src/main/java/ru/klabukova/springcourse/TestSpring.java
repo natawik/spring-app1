74767eb3d1a3c0697dd043add876681a956f63a5
natawik <natawik.spb@gmail.com>
2020-11-11 11:29:31 +0300

    create object and dep injection with xml

--- src/main/java/ru/klabukova/springcourse/TestSpring.java
+++ src/main/java/ru/klabukova/springcourse/TestSpring.java
@@ -13,6 +13,7 @@ public class TestSpring {
 		
 		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		musicPlayer.playMusic();
+		musicPlayer.playMusic();
 		System.out.println(musicPlayer.getName());
 		System.out.println(musicPlayer.getVolume());
 		
